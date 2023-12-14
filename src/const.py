3243fedb9d9ff0b8cd9086979527dd441f51fcac
MercurialRed <mercurialred54@gmail.com>
2023-12-14 22:00:26 +0200

    - added the requirements file - added const.py with screen and board sizes specified - added main.py which runs the game window and keeps it on loop - added game.py where the background of the board specified

---
+++ src/const.py
@@ -0,0 +1,11 @@
+"""
+This file stores the main constants of the game, such as screen and board sizes
+"""
+# Screen dimensions
+WIDTH = 800
+HEIGHT = 800
+
+# Board dimensions
+ROWS = 8
+COLS = 8
+SQSIZE = WIDTH // COLS
