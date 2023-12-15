de8d5674b1cd517f7198808cb12a967b8e1175f4
MercurialRed <mercurialred54@gmail.com>
2023-12-15 18:04:44 +0200

    - added square.py - added board.py which divides the board into rows and columns

---
+++ src/square.py
@@ -0,0 +1,7 @@
+class Square:
+
+    def __init__(self, row, col, piece=None):  # a square may contain a piece or not
+        self.row = row
+        self.col = col
+        self.piece = piece
+        
