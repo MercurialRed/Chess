323e57ac1caca93da814f39a50820d744a7671dd
MercurialRed <mercurialred54@gmail.com>
2023-12-24 19:26:49 +0200

    - added move.py - updated board.py with visual moves for all pieces - updated game.py to show moves - updated main.py to show moves - updated piece.py to add moves - updated squares.py to check the square occupation

--- src/game.py
+++ src/game.py
@@ -39,3 +39,19 @@ class Game:
                         img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
                         piece.texture_rect = img.get_rect(center=img_center)
                         surface.blit(img, piece.texture_rect)
+
+    def show_moves(self, surface):
+        if self.dragger.dragging:
+            piece = self.dragger.piece
+
+            # loop all valid moves
+            for move in piece.moves:
+                # color
+                if (move.final.row + move.final.col) % 2 == 0:
+                    color = '#C86464'
+                else:
+                    color = '#C84646'
+                # rect
+                rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
+                # blit
+                pygame.draw.rect(surface, color, rect)
