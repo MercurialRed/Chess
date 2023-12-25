4954a300a60933a5edaf1fee756f46f7f854d712
MercurialRed <mercurialred54@gmail.com>
2023-12-25 18:17:56 +0200

    added features to show the last move, square hovering and player turns - added move method - added valid move method

--- src/game.py
+++ src/game.py
@@ -8,6 +8,8 @@ from dragger import Dragger
 class Game:
 
     def __init__(self):
+        self.next_player = "white"
+        self.hovered_sqr = None
         self.board = Board()
         self.dragger = Dragger()
 
@@ -55,3 +57,37 @@ class Game:
                 rect = (move.final.col * SQSIZE, move.final.row * SQSIZE, SQSIZE, SQSIZE)
                 # blit
                 pygame.draw.rect(surface, color, rect)
+
+    def show_last_move(self, surface):
+        if self.board.last_move:
+            initial = self.board.last_move.initial
+            final = self.board.last_move.final
+
+            for pos in [initial, final]:
+                # color
+                color = (244, 247, 116) if (pos.row + pos.col) % 2 == 0 else (172, 195, 51)
+                # rect
+                rect = (pos.col * SQSIZE, pos.row * SQSIZE, SQSIZE, SQSIZE)
+                # blit
+                pygame.draw.rect(surface, color, rect)
+
+    def show_hover(self, surface):
+        if self.hovered_sqr:
+            # color
+            color = (255, 255, 0)
+            # rect
+            rect = (self.hovered_sqr.col * SQSIZE, self.hovered_sqr.row * SQSIZE, SQSIZE, SQSIZE)
+            # blit
+            pygame.draw.rect(surface, color, rect, width=3)
+
+    # Other methods
+
+    def next_turn(self):
+        if self.next_player == 'black':
+            self.next_player = 'white'
+        else:
+            self.next_player = 'black'
+
+    def set_hover(self, row, col):
+        self.hovered_sqr = self.board.squares[row][col]
+
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import sys
 
 from const import *
 from game import Game
+from square import Square
+from  move import Move
 
 
 class Main:
@@ -22,7 +24,9 @@ class Main:
         while True:
             # Show methods
             game.show_bg(screen)
+            game.show_last_move(screen)
             game.show_moves(screen)
+            game.show_hover(screen)
             game.show_pieces(screen)
 
             if dragger.dragging:  # board clipping fix
@@ -40,26 +44,57 @@ class Main:
                     # If clicked square has a piece?
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col].piece
-                        board.calc_moves(piece, clicked_row, clicked_col)
-                        dragger.save_initial(event.pos)
-                        dragger.drag_piece(piece)
-                        # Show methods
-                        game.show_bg(screen)
-                        game.show_moves(screen)
-                        game.show_pieces(screen)
+                        # Valid piece (color) ?
+                        if piece.color == game.next_player:
+                            board.calc_moves(piece, clicked_row, clicked_col)
+                            dragger.save_initial(event.pos)
+                            dragger.drag_piece(piece)
+                            # Show methods
+                            game.show_bg(screen)
+                            game.show_last_move(screen)
+                            game.show_moves(screen)
+                            game.show_pieces(screen)
 
                 # Mouse motion
                 elif event.type == pygame.MOUSEMOTION:
+                    motion_row = event.pos[1] // SQSIZE
+                    motion_col = event.pos[0] // SQSIZE
+                    game.set_hover(motion_row, motion_col)
+
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
                         # Show methods
                         game.show_bg(screen)
+                        game.show_last_move(screen)
                         game.show_moves(screen)
                         game.show_pieces(screen)
+                        game.show_hover(screen)
                         dragger.update_blit(screen)
 
                 # Click release
                 elif event.type == pygame.MOUSEBUTTONUP:
+
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+
+                        released_row = dragger.mouseY // SQSIZE
+                        released_col = dragger.mouseX // SQSIZE
+
+                        # Create possible move
+                        initial = Square(dragger.initial_row, dragger.initial_col)
+                        final = Square(released_row, released_col)
+                        move = Move(initial, final)
+
+                        # Valid move ?
+                        if board.valid_move(dragger.piece, move):
+                            board.move(dragger.piece, move)
+                            # Show methods
+                            game.show_bg(screen)
+                            game.show_last_move(screen)
+                            game.show_pieces(screen)
+                            # Next turn
+                            game.next_turn()
+
                     dragger.undrag_piece()
 
                 # Quit application
